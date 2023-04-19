8a232f2f51003a01124d4ee723da996056efdb5a
Lucas Ribeiro <marquesribeirolucas@gmail.com>
2023-04-19 19:12:51 -0300

    Criado aula 3

---
+++ aula 3.py
@@ -0,0 +1,6 @@
+nota1 = input("Informe a nota 1: ")
+nota2 = input("Informe a nota 2: ")
+nota3 = input("Informe a nota 3: ")
+
+media = (nota1 + nota2 + nota3)/3
+print(f"A média final é: (media)\n")
