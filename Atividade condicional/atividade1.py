95f7e9c678679bae80eeda410bb8a56ed1e6dfb0
Lucas Ribeiro <marquesribeirolucas@gmail.com>
2023-04-20 21:07:31 -0300

    atividade 1 condicional concluída

---
+++ Atividade condicional/atividade1.py
@@ -0,0 +1,6 @@
+valor = int(input('informe um valor qualquer: '))
+
+if valor >=100 and valor<=200:
+    print(f'O valor {valor} está entre 100 e 200\n')
+else:
+    print(f'O valor {valor} não está entre 100 e 200\n')
