0b9455f6158a3c48e4cfe15e3c880377ada5983a
Lucas Ribeiro <marquesribeirolucas@gmail.com>
2023-04-24 21:15:52 -0300

    Iniciada atividade para

---
+++ Atividade Para/atv1.py
@@ -0,0 +1,7 @@
+soma = 0
+media = 0
+total = 0
+
+for contador in range(50,70):
+    total = total + 1
+print(f'O total é {total}\n')
---
+++ aula 6-para.py
@@ -0,0 +1,4 @@
+for contador in range(1,11):
+    print(contador)
+for contador in range(10,0,-1):# o -1 indica que a contagem irá começar do maior para o menor
+    print(contador)
