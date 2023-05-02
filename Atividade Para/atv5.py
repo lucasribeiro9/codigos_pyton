44a70d3a42c7490cb2308b3f9ade221ba605e3a1
Lucas Ribeiro <marquesribeirolucas@gmail.com>
2023-05-02 20:41:46 -0300

    Finalizado atividade PARA

--- Atividade Para/atv5.py
+++ Atividade Para/atv5.py
@@ -1,4 +1,13 @@
 valor = int(input('Insira um valor qualquer: '))
 
+maior = 0
+posicao = 0
+
 for contador in range(1,valor+1):
+    item = int(input('insira um valor: '))
+    if item >= maior:
+        maior = item
+        posicao = contador
+        
+print(f'O maior valor é {maior} e está na posição {posicao}\n')
     
