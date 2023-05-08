1e48899dd19474e3980651d78cd734fca773e690
Lucas Ribeiro <marquesribeirolucas@gmail.com>
2023-05-08 20:24:33 -0300

    Criado aula tupla

---
+++ aula8-tupla.py
@@ -0,0 +1,20 @@
+lanche = ('pizza', 'hotdog', 'refri', 'batata')
+print(lanche)
+print(type(lanche)) #estou mostrando o tipo da variável
+
+print(lanche[1])
+print(f' o total de lanches é {len(lanche)}\n') #length
+
+#lanche[2] = 'Suco'#
+
+for contador in range(0,4):
+    print(lanche[contador])
+    
+print('*'*100)    
+for item in lanche:
+    print(item)
+    
+print('*'*100)
+#enumerate serve para permitir acessar os índices da tupla. Já a variável indice, irá armazenar os valores do índice    
+for indice,elemento in enumerate(lanche):
+    print(f'{indice} = {elemento}')
